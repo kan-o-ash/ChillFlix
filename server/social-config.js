1730f0974d07065ed9b8cfa027090aaa26ccd79e
Ryan Callen <callenrm@miamioh.edu>
2015-09-25 21:42:37 -0400

    Added Initial Meteor Files

---
+++ server/social-config.js
@@ -0,0 +1,10 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+ 
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '646889445451102',
+    secret: 'd0b9569bb2bb555a80b5516121e71440'
+});
+
