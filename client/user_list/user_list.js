50f34e45dc78f984474c6af3c42ab4d6a87daacb
Ashkan <ashkan.pk@gmail.com>
2015-09-26 19:28:24 -0400

    Updated cards UI

--- client/user_list/user_list.js
+++ client/user_list/user_list.js
@@ -1,3 +1,16 @@
+Template.user_list.onRendered(function () {
+    $( "#but-yes" ).click(function() {
+      $( ".panel" ).animate({
+        'left': "200%"
+      }, 500);
+    });
+    $( "#but-nope" ).click(function() {
+      $( ".panel" ).animate({
+        'right': "200%"
+      }, 500);
+    });
+})
+
 Template.user_list.helpers({  
     users: function() {
         var users = Meteor.users.find({'services.facebook.id': {$ne : Meteor.user().services.facebook.id}}).map(function(doc, index, cursor) {
@@ -7,12 +20,17 @@ Template.user_list.helpers({
         return users
     },
     
+   
+    current_person: function() {
+
+    }
+});
+
+Template.movie_list.helpers({
     user_likes: function() {
-        
         console.log(this); 
         return MovieLikes.find({"user_id": this._id});        
     }
-       
 });
 
 Template.movie_title.helpers({
