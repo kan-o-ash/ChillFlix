370492b910f610161978afaa9f2cf1b72390e8a1
Ryan Callen <callenrm@miamioh.edu>
2015-09-26 03:41:39 -0400

    Dynamic templates -- porting

--- client/helpers.js
+++ client/helpers.js
@@ -1,3 +1,27 @@
+//HELPERS TO MANAGE NAVIGATION
+Meteor.startup(function () {
+  Session.setDefault("templateName", "index");
+});
+
+Template.body.helpers({
+  template_name: function(){
+    return Session.get("templateName");
+  }
+});
+
+Template.body.events({
+  "click .user_list": function() {
+    Session.set("templateName", "user_list");
+  },
+  "click .edit_profile": function() {
+     Session.set("templateName", "edit_profile");
+  }
+});
+
+
+
+
+//HELPERS FOR ALL OTHER TEMPLATES
 Template.login.events({
     'click #facebook-login': function(event) {
         Meteor.loginWithFacebook({}, function(err){
