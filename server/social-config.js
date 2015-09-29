5e633863350ac9ca015836094199ae56054b2638
Ryan Callen <callenrm@miamioh.edu>
2015-09-29 14:09:37 -0400

    Edited Login UI components
    -If no one is logged in, the main page displays a new splash message
    and login button
    -There is no longer a login/logout button in the nav bar
    -Users logout on their own profile page
    -Added the TV back in the logo :)))))

--- server/social-config.js
+++ server/social-config.js
@@ -1,3 +1,4 @@
+
 ServiceConfiguration.configurations.remove({
     service: 'facebook'
 });
@@ -7,4 +8,19 @@ ServiceConfiguration.configurations.insert({
     appId: '889420544483140',
     secret: 'a02489a62cf5c98b08e3292d5bef8692'
 });
+
+
+//Uncomment the below code to access API key for localhost testing
+
+/*
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+ 
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '646889445451102',
+    secret: 'd0b9569bb2bb555a80b5516121e71440'
+});
+*/
 
